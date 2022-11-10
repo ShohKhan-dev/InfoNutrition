cbc1949cb9ac9d603c574f575a34a59b4fae23ca
ShohKhan-dev <shoh.hamraqulov@gmail.com>
2022-11-10 03:40:21 +0300

    Ready to Deploy

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include("nutrition.urls")),
+
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
